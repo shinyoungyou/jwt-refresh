572fa450ca107114bd41e1b40f5da93b9a94b4c4
AustynYou <austyn.you@gmail.com>
2023-08-15 11:40:13 -0700

    useToggle for persist boolean value

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,14 +1,14 @@
 import { useRef, useState, useEffect } from 'react';
 import useAuth from '../hooks/useAuth';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
-import useLocalStorage from '../hooks/useLocalStorage';
 import useInput from '../hooks/useInput';
+import useToggle from '../hooks/useToggle';
 
 import axios from '../api/axios';
 const LOGIN_URL = '/auth';
 
 const Login = () => {
-    const { setAuth, persist, setPersist } = useAuth();
+    const { setAuth } = useAuth();
 
     const navigate = useNavigate();
     const location = useLocation();
@@ -17,9 +17,10 @@ const Login = () => {
     const userRef = useRef();
     const errRef = useRef();
 
-    const [user, resetUser, userAttributs] = useInput('user',''); // useState('');
+    const [user, resetUser, userAttributs] = useInput('user', ''); // useState('');
     const [pwd, setPwd] = useState('');
     const [errMsg, setErrMsg] = useState('');
+    const [check, toggleCheck] = useToggle('persist', false);
 
     useEffect(() => {
         userRef.current.focus();
@@ -63,14 +64,6 @@ const Login = () => {
         }
     }
 
-    const togglePersist = () => {
-        setPersist(prev => !prev);
-    }
-
-    useEffect(() => {
-        localStorage.setItem("persist", persist);
-    }, [persist])
-
     return (
 
         <section>
@@ -100,8 +93,8 @@ const Login = () => {
                     <input 
                         type="checkbox" 
                         id="persist"
-                        onChange={togglePersist}
-                        checked={persist}
+                        onChange={toggleCheck}
+                        checked={check}
                     />
                      <label htmlFor="persist">Trust This Device</label>
                 </div>
